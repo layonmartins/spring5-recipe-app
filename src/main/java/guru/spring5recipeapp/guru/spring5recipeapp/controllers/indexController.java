45a0d1caf026e47f3c1b21f90a7eabc42e791061
layonmartins <layon.fonseca@eldorado.org.br>
2022-10-25 12:21:40 -0300

    Aula 101: Create Index Page

---
+++ src/main/java/guru/spring5recipeapp/guru/spring5recipeapp/controllers/indexController.java
@@ -0,0 +1,14 @@
+package guru.spring5recipeapp.guru.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+
+}
---
+++ src/test/java/guru/spring5recipeapp/guru/spring5recipeapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring5recipeapp.guru.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
